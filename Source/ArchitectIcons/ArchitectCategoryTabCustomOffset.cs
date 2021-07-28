b95d2eac20189e7e43b1d83f8c5cce24697d0f40
Marcin Dobrowolski <marcin212@protonmail.com>
2021-07-28 22:29:43 +0200

    Update to 1.2 and 1.3

--- Source/ArchitectIcons/ArchitectCategoryTabCustomOffset.cs
+++ Source/ArchitectIcons/ArchitectCategoryTabCustomOffset.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using RimWorld;
 using Verse;
 
@@ -6,23 +9,27 @@ namespace ArchitectIcons
 {
     public class ArchitectCategoryTabCustomOffset : ArchitectCategoryTab
     {
-        public ArchitectCategoryTabCustomOffset(DesignationCategoryDef def) : base(def)
+        private readonly ReflectionUtils.FieldAccess<ArchitectCategoryTab, Func<Gizmo, bool>> Parent_shouldHighLightGizmoFunc;
+        private readonly ReflectionUtils.FieldAccess<ArchitectCategoryTab, Func<Gizmo, bool>> Parent_shouldLowLightGizmoFunc;
+
+        public ArchitectCategoryTabCustomOffset(DesignationCategoryDef def, QuickSearchFilter quickSearchFilter) : base(def, quickSearchFilter)
         {
+            Parent_shouldHighLightGizmoFunc = new ReflectionUtils.FieldAccess<ArchitectCategoryTab, Func<Gizmo, bool>>(this, "shouldHighLightGizmoFunc");
+            Parent_shouldLowLightGizmoFunc = new ReflectionUtils.FieldAccess<ArchitectCategoryTab, Func<Gizmo, bool>>(this, "shouldLowLightGizmoFunc");
+
         }
 
-        public void DesignationTabOnGUI()
+        public void DesignationTabOnGUI(Designator forceActivatedCommand)
         {
             if (Find.DesignatorManager.SelectedDesignator != null)
-                Find.DesignatorManager.SelectedDesignator.DoExtraGuiControls(0.0f,
-                    (float) ((double) (UI.screenHeight - 35) -
-                             (double) ((RimWorld.MainTabWindow_Architect) MainButtonDefOf.Architect.TabWindow)
-                             .WinHeight - 270.0));
+                Find.DesignatorManager.SelectedDesignator.DoExtraGuiControls(0.0f, (float)((double)(UI.screenHeight - 35) - (double)((RimWorld.MainTabWindow_Architect)MainButtonDefOf.Architect.TabWindow).WinHeight - 270.0));
+            Func<Gizmo, bool> customActivatorFunc = forceActivatedCommand == null ? (Func<Gizmo, bool>)null : (Func<Gizmo, bool>)(cmd => cmd == forceActivatedCommand);
             Gizmo mouseoverGizmo;
-            GizmoGridDrawer.DrawGizmoGrid(this.def.ResolvedAllowedDesignators.Cast<Gizmo>(), 210f + 2 * 16,
-                out mouseoverGizmo);
+            GizmoGridDrawer.DrawGizmoGrid((IEnumerable<Gizmo>)this.def.ResolvedAllowedDesignators, 210f + 2 * 16f, out mouseoverGizmo, customActivatorFunc, this.Parent_shouldHighLightGizmoFunc.Value, this.Parent_shouldLowLightGizmoFunc.Value);
             if (mouseoverGizmo == null && Find.DesignatorManager.SelectedDesignator != null)
-                mouseoverGizmo = (Gizmo) Find.DesignatorManager.SelectedDesignator;
-            DoInfoBox(InfoRect, (Designator) mouseoverGizmo);
+                mouseoverGizmo = (Gizmo)Find.DesignatorManager.SelectedDesignator;
+            this.DoInfoBox(ArchitectCategoryTab.InfoRect, (Designator)mouseoverGizmo);
         }
+
     }
 }
--- Source/ArchitectIcons/MainTabWindow_Architect.cs
+++ Source/ArchitectIcons/MainTabWindow_Architect.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using RimWorld;
 using UnityEngine;
 using Verse;
@@ -9,9 +10,22 @@ namespace ArchitectIcons
 {
     public class MainTabWindow_Architect : RimWorld.MainTabWindow_Architect
     {
-        private List<ArchitectCategoryTabCustomOffset> desPanelsCached;
+        private static readonly Color NoMatchColor = Color.grey;
         public const float WinWidth = 200f + 2 * 16;
-        private bool swaped = false;
+
+        private readonly ReflectionUtils.FieldAccess<RimWorld.MainTabWindow_Architect, List<ArchitectCategoryTab>> Parent_desPanelsCached;
+        private readonly ReflectionUtils.FieldAccess<RimWorld.MainTabWindow_Architect, Designator> Parent_forceActivatedCommand;
+        private readonly ReflectionUtils.FieldAccess<RimWorld.MainTabWindow_Architect, bool> Parent_didInitialUnfocus;
+        private readonly ReflectionUtils.FieldAccess<RimWorld.MainTabWindow_Architect, QuickSearchWidget> Parent_quickSearchWidget;
+
+        private readonly ReflectionUtils.MethodAccess<RimWorld.MainTabWindow_Architect, ArchitectCategoryTabCustomOffset> Parent_OpenTab;
+        private readonly ReflectionUtils.MethodAccess<RimWorld.MainTabWindow_Architect, ArchitectCategoryTab> Parent_CacheSearchState;
+
+        public override void ExtraOnGUI()
+        {
+            Parent_OpenTab.Invoke()?.DesignationTabOnGUI(this.Parent_forceActivatedCommand.Value);
+            this.Parent_forceActivatedCommand.Value = (Designator) null;
+        }
 
         public override Vector2 RequestedTabSize
         {
@@ -22,36 +36,51 @@ namespace ArchitectIcons
         {
             get
             {
-                if (desPanelsCached == null)
+                if (Parent_desPanelsCached.Value == null)
                     this.CacheDesPanels();
-                return (float) Mathf.CeilToInt((float) desPanelsCached.Count / 2f) * 32f;
+                return (float)((double)Mathf.CeilToInt((float)this.Parent_desPanelsCached.Value.Count / 2f) * 32.0 + 28.0);
             }
         }
 
         public MainTabWindow_Architect()
         {
+            Parent_desPanelsCached = new ReflectionUtils.FieldAccess<RimWorld.MainTabWindow_Architect, List<ArchitectCategoryTab>>(this, "desPanelsCached");
+            Parent_forceActivatedCommand = new ReflectionUtils.FieldAccess<RimWorld.MainTabWindow_Architect, Designator>(this, "forceActivatedCommand");
+            Parent_didInitialUnfocus = new ReflectionUtils.FieldAccess<RimWorld.MainTabWindow_Architect, bool>(this, "didInitialUnfocus");
+            Parent_quickSearchWidget = new ReflectionUtils.FieldAccess<RimWorld.MainTabWindow_Architect, QuickSearchWidget>(this, "quickSearchWidget");
+
+            Parent_OpenTab = new ReflectionUtils.MethodAccess<RimWorld.MainTabWindow_Architect, ArchitectCategoryTabCustomOffset>(this, "OpenTab");
+            Parent_CacheSearchState = new ReflectionUtils.MethodAccess<RimWorld.MainTabWindow_Architect, ArchitectCategoryTab>(this, "CacheSearchState");
+
             this.CacheDesPanels();
         }
 
-        public override void ExtraOnGUI()
+        public override void PreOpen()
         {
-            if (this.selectedDesPanel == null)
-                return;
-            (selectedDesPanel as ArchitectCategoryTabCustomOffset)?.DesignationTabOnGUI();
+            CacheDesPanelsWithCheck();
+            base.PreOpen();
+        }
+
+        private void CacheDesPanelsWithCheck()
+        {
+            if (this.Parent_desPanelsCached.Value == null)
+            {
+                CacheDesPanels();
+            }
         }
 
         private void CacheDesPanels()
         {
-            this.desPanelsCached = new List<ArchitectCategoryTabCustomOffset>();
+            Parent_desPanelsCached.Value = new List<ArchitectCategoryTab>();
             foreach (DesignationCategoryDef def in (IEnumerable<DesignationCategoryDef>)
                 DefDatabase<DesignationCategoryDef>.AllDefs.OrderByDescending<DesignationCategoryDef, int>(
                     (Func<DesignationCategoryDef, int>) (dc => dc.order)))
-                desPanelsCached.Add(new ArchitectCategoryTabCustomOffset(def));
+                Parent_desPanelsCached.Value.Add(new ArchitectCategoryTabCustomOffset(def, Parent_quickSearchWidget.Value.filter));
 
             List<string> listTab = new List<string>();
-            for (int index = 0; index < desPanelsCached.Count; ++index)
+            for (int index = 0; index < Parent_desPanelsCached.Value.Count; ++index)
             {
-                listTab.Add(desPanelsCached[index].def.defName);
+                listTab.Add(Parent_desPanelsCached.Value[index].def.defName);
             }
 
             try
@@ -61,35 +90,63 @@ namespace ArchitectIcons
             catch (Exception e) { };
         }
 
+
         public override void DoWindowContents(Rect inRect)
         {
-            SetInitialSizeAndPosition();
+
             Text.Font = GameFont.Small;
             float width = inRect.width / 2f;
             float num1 = 0.0f;
             float num2 = 0.0f;
-            for (int index = 0; index < desPanelsCached.Count; ++index)
+            float a = 0.0f;
+            ArchitectCategoryTab architectCategoryTab = this.Parent_OpenTab.Invoke();
+            if (KeyBindingDefOf.Accept.KeyDownEvent)
+            {
+                if (this.Parent_quickSearchWidget.Value.filter.Active && architectCategoryTab != null && architectCategoryTab.UniqueSearchMatch != null)
+                    this.Parent_forceActivatedCommand.Value = architectCategoryTab.UniqueSearchMatch;
+                else
+                    this.Close(true);
+                Event.current.Use();
+            }
+            for (int index = 0; index < this.Parent_desPanelsCached.Value.Count; ++index)
             {
                 Rect rect = new Rect(num1 * width, num2 * 32f, width, 32f);
                 ++rect.height;
-                if ((double) num1 == 0.0)
+                if ((double)num1 == 0.0)
                     ++rect.width;
-
-                if (Widgets.ButtonTextSubtle(rect, desPanelsCached[index].def.LabelCap, 0.0f, 24f,
-                    SoundDefOf.Mouseover_Category, new Vector2(-1, -1)))
-                    this.ClickedCategory(desPanelsCached[index]);
-                if (this.selectedDesPanel != desPanelsCached[index])
-                    UIHighlighter.HighlightOpportunity(rect, desPanelsCached[index].def.cachedHighlightClosedTag);
+                ArchitectCategoryTab Pan = this.Parent_desPanelsCached.Value[index];
+                Color? labelColor = Pan.AnySearchMatches ? new Color?() : new Color?(NoMatchColor);
+                string labelCap = (string)Pan.def.LabelCap;
+                if (Widgets.ButtonTextSubtle(rect, labelCap, 0.0f, 24f, SoundDefOf.Mouseover_Category, new Vector2(-1f, -1f), labelColor, this.Parent_quickSearchWidget.Value.filter.Active && architectCategoryTab == Pan))
+                    this.ClickedCategory(Pan);
+                if (this.selectedDesPanel != Pan)
+                    UIHighlighter.HighlightOpportunity(rect, Pan.def.cachedHighlightClosedTag);
+                a = Mathf.Max(a, rect.yMax);
                 ++num1;
-                if ((double) num1 > 1.0)
+                if ((double)num1 > 1.0)
                 {
                     num1 = 0.0f;
                     ++num2;
                 }
-
                 GUI.DrawTexture(new Rect(rect.position + new Vector2(4, (32 - 16) / 2), new Vector2(16, 16)),
-                    Resources.FindArchitectTabCategoryIcon(desPanelsCached[index].def.defName));
+                         Resources.FindArchitectTabCategoryIcon(Parent_desPanelsCached.Value[index].def.defName));
             }
+
+            this.Parent_quickSearchWidget.Value.OnGUI(new Rect(0.0f, a + 1f, inRect.width, 24f), new Action(() =>
+            {
+                CacheDesPanelsWithCheck();
+                this.Parent_CacheSearchState.Invoke();
+            }));
+            if (this.Parent_didInitialUnfocus.Value)
+                return;
+            UI.UnfocusCurrentControl();
+            Parent_didInitialUnfocus.Value = true;
+        }
+
+        public override void OnCancelKeyPressed()
+        {
+            CacheDesPanelsWithCheck();
+            base.OnCancelKeyPressed();
         }
     }
 }
