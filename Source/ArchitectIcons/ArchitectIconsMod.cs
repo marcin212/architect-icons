5e796588f716d604a35f6af6b0da70e15f16af42
Marcin Dobrowolski <marcin212@protonmail.com>
2022-10-22 20:37:51 +0200

    update to RimWorld 1.4

--- Source/ArchitectIcons/ArchitectIconsMod.cs
+++ Source/ArchitectIcons/ArchitectIconsMod.cs
@@ -18,7 +18,7 @@ namespace ArchitectIcons
             var harm = new Harmony("com.bymarcin.architecticons");
             harm.Patch(AccessTools.PropertyGetter(typeof(MainTabWindow_Architect), nameof(MainTabWindow_Architect.RequestedTabSize)),
                 postfix: new HarmonyMethod(GetType(), nameof(AddWidth)));
-            harm.Patch(AccessTools.Method(typeof(MainTabWindow_Architect), nameof(MainTabWindow_Architect.DoWindowContents)),
+            harm.Patch(AccessTools.Method(typeof(MainTabWindow_Architect), AccessTools.Method(typeof(MainTabWindow_Architect), "DoCategoryButton").Name),
                 transpiler: new HarmonyMethod(GetType(), nameof(ReplaceArchitectButton)));
             harm.Patch(AccessTools.Method(typeof(ArchitectCategoryTab), nameof(ArchitectCategoryTab.DesignationTabOnGUI)),
                 transpiler: new HarmonyMethod(GetType(), nameof(OffsetGizmos)));
@@ -41,6 +41,7 @@ namespace ArchitectIcons
             if (defNamesFromLabels.TryGetValue(label, out var defName)) return defName;
             defName = DefDatabase<DesignationCategoryDef>.AllDefs.First(def => def.LabelCap == label).defName;
             defNamesFromLabels[label] = defName;
+            Logger.Debug(defName + "=>" + label);
             return defName;
         }
 
@@ -52,7 +53,10 @@ namespace ArchitectIcons
         {
             foreach (var instruction in instructions)
             {
-                if (instruction.opcode == OpCodes.Ldc_R4 && (float) instruction.operand == 210f) instruction.operand = (float) instruction.operand + ADD_WIDTH;
+                if (instruction.opcode == OpCodes.Ldc_R4 && (float)instruction.operand == 210f)
+                {
+                    instruction.operand = (float) instruction.operand + ADD_WIDTH;
+                }
 
                 yield return instruction;
             }
